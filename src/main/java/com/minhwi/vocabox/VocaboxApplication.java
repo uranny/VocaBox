cc1ea01c4f9a6201f00eab7cd39bdbbf22a57428
uranny <164876325+uranny@users.noreply.github.com>
2025-11-20 17:03:56 +0900

    initial commit

---
+++ src/main/java/com/minhwi/vocabox/VocaboxApplication.java
@@ -0,0 +1,13 @@
+package com.minhwi.vocabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocaboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhwi/vocabox/VocaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhwi.vocabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
